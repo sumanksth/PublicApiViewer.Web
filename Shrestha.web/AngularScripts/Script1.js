da39e7dd6d1508de1cd852117130be0a8f21ddb1
sumanksth <suman.kaji@gmail.com>
2015-07-20 22:45:21 -0500

    Public Api Viewer

---
+++ Shrestha.web/AngularScripts/Script1.js
@@ -0,0 +1,15 @@
+﻿function firstController($scope,$http) {
+    $scope.name = "Hello";
+    var OnSuccess = function (response) {
+        $scope.user = response.data;
+    };
+    var OnError = function () {
+        $scope.error = "som thing wong - wee too lou";
+    };
+    $scope.searchGit = function () {
+                        $http.get("https://api1.github.com/users/angular")
+                         .then(OnSuccess, OnError)
+    };
+    
+    
+};
